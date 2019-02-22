89009d235ccfe0d7d9526cb3989440efa7686e39
Roger Russel <roger@rrussel.org>
2019-02-21 23:02:28 -0300

    separete controllers from socket start

--- internal/pkg/sockets/start.go
+++ internal/pkg/sockets/start.go
@@ -4,40 +4,22 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 
-	"github.com/gorilla/websocket"
+	"github.com/alanleite/hex-game-server/internal/pkg/controllers/echo"
 )
-
-var addr = flag.String("addr", "localhost:8080", "http service address")
-var upgrader = websocket.Upgrader{} // use default options
-
-func echo(w http.ResponseWriter, r *http.Request) {
-	c, err := upgrader.Upgrade(w, r, nil)
-	if err != nil {
-		log.Print("upgrade:", err)
-		return
-	}
-	defer c.Close()
-	for {
-		mt, message, err := c.ReadMessage()
-		if err != nil {
-			log.Println("read:", err)
-			break
-		}
-		log.Printf("recv: %s", message)
-		err = c.WriteMessage(mt, message)
-		if err != nil {
-			log.Println("write:", err)
-			break
-		}
-	}
-}
 
 func Start() {
 
+	var addr = flag.String("addr", os.Getenv("URL"), "http service address")
+
 	flag.Parse()
 	log.SetFlags(0)
-	http.HandleFunc("/echo", echo)
+	routes()
 	log.Fatal(http.ListenAndServe(*addr, nil))
 
+}
+
+func routes() {
+	http.HandleFunc("/echo", echo.Index)
 }
