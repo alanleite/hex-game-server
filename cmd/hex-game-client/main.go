4fa75c83c381c17acdd38d80860fd2bb7c4a1e0d
Roger Russel <roger@rrussel.org>
2019-02-21 23:00:17 -0300

    start to use the env on realize.yml

--- cmd/hex-game-client/main.go
+++ cmd/hex-game-client/main.go
@@ -11,7 +11,7 @@ import (
 	"github.com/gorilla/websocket"
 )
 
-var addr = flag.String("addr", "localhost:8080", "http service address")
+var addr = flag.String("addr", os.Getenv("URL"), "http service address")
 
 func main() {
 	flag.Parse()
